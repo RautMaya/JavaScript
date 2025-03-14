45d90eeee6d4121e59fcca8e1103877a77535fb3
RautMaya <suryawanshi28@gmail.com>
2025-03-14 15:56:46 +0530

    Math Oerator

---
+++ Practice Assignment/Math Operator in JavaScript/script.js
@@ -0,0 +1,17 @@
+const num1 = 10
+const num2 = '25'
+
+const Equl = num1!==num2
+const Equal3 = num1===num2
+
+const num3 = +'10'  ///Conversion Of String To Number
+const num4 = 10
+
+const Using2Equal = num3==num4
+const Using3Equal = num3===num4
+
+const Number = +'15'
+const Number1 = 25
+
+const Greatter = Number1 <= Number
+
