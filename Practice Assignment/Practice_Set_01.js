34f593345104e684bf428e2496b901d4684cae04
RautMaya <suryawanshi28@gmail.com>
2025-03-14 15:55:58 +0530

    Practice Some Basic Questions

---
+++ Practice Assignment/Practice_Set_01.js
@@ -0,0 +1,33 @@
+///Questation 1
+let a = "Punam"
+let b = 8;
+
+console.log(a+b)
+
+///Questation 1
+
+console.log(typeof(a+b))
+
+///Questation 1
+
+const a1 = {
+    name : "Maya",
+    RollNo : 101,
+    isPrinciple : false
+}
+console.log(a1.RollNo);
+console.log(a1.name);
+
+a1['Friend'] = "Sakshi"
+
+console.log(typeof(a1))
+console.log(a1)
+
+const Disc = {
+    Life : "School Life",
+    Happy : "Be happy",
+    Skills : "Learn"
+
+}
+console.log(Disc.Happy)
+console.log(Disc['Life'])  /// This to be used To access Data
