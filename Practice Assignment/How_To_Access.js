70156ef5ce1e90894de5ac311be19fc0f21cf41d
RautMaya <suryawanshi28@gmail.com>
2025-03-15 18:35:35 +0530

    How To Access

---
+++ Practice Assignment/How_To_Access.js
@@ -0,0 +1,15 @@
+let Student
+{
+    let Roll = 101;
+    var Name ="Maya";
+
+    console.log(this.Roll);
+
+    ///console.log(Student.["Name"]);
+    function Mark()
+    {
+        let Col = "Maya";
+        console.log("Hellow ," + Student.Roll);
+    }
+    console.log("Hellow ," + Mark.Col);
+};
