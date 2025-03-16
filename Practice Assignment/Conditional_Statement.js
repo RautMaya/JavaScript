9451e39494944e0a72ae3722a72ad541684b7d9b
RautMaya <suryawanshi28@gmail.com>
2025-03-16 15:23:47 +0530

    Conditional Statement

---
+++ Practice Assignment/Conditional_Statement.js
@@ -0,0 +1,39 @@
+
+let age = 12;
+
+if((age >= 10) && (age < 20))
+{
+    console.log("Age is lies Between 10 and 20")
+}
+else
+{
+    console.log("Age Does not lies Between 10 and 20")
+}
+
+let Day;
+
+///prompt("Enter a number");
+
+switch(Day)
+{
+   
+
+    case 1 : console.log("Sunday");
+    break;
+    case 2 : console.log("Monday");
+    break;
+    case 3 : console.log("Tuesday");
+    break;
+    case 4 : console.log("Wensday");
+    break;
+    case 5 : console.log("Thruesday");
+    break;
+    case 6 : console.log("Friday");
+    break;
+    case 7 : console.log("Saturday");
+    break;
+
+    case 8 : console.log("Invalid Number");
+    break;
+
+}
