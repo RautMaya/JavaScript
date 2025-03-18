3d9f197d11bb073168b6a47f64f3803dbe0225a0
RautMaya <suryawanshimaya28@gmail.com>
2025-03-18 16:32:19 +0530

    Array Loop

---
+++ Practice Assignment/Array_Loop.js
@@ -0,0 +1,32 @@
+let num = [1,2,3,4,5,6]
+
+for(let i=0;i<num.length;i++)
+{
+    console.log(num[i])
+}
+
+/// ForEach Loop
+
+num.forEach((element) => {
+    console.log(element*element)
+})
+
+///Array.From
+
+let Name = "Maya"
+let arr =Array.from(Name)
+console.log(arr)
+
+///For_Of loop
+
+for(let i of num)
+{
+    console.log(i)
+}
+
+///For_in Loop
+
+for(let i in num)
+{
+    console.log(i)
+}
