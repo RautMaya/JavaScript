93bd5121d0eac7b6f32174d8cfd25719db4f16f2
RautMaya <suryawanshimaya28@gmail.com>
2025-03-18 16:34:55 +0530

    Array Methods

--- Practice Assignment/Array_Methods.js
+++ Practice Assignment/Array_Methods.js
@@ -26,7 +26,7 @@ console.log(Student_Marks.length)
 let Mark_1 = [11,712,193]
 let Mark_2 = [241555555555,522,723]
 let Mark_3 = [831,312,303]
-let Mark_4 = [831,312,303]
+
 
 
 let New_Array = Mark_1.concat(Mark_2,Mark_3)
