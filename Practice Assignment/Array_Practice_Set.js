44cfdebb609ccd506a8225a040bd6f0f54437752
RautMaya <suryawanshimaya28@gmail.com>
2025-03-19 14:22:29 +0530

    Array_Practice_Set.js

---
+++ Practice Assignment/Array_Practice_Set.js
@@ -0,0 +1,29 @@
+///Question 1
+const arr = [10,3,60,5,56,98,50]
+
+arr.push(22,33,44,55)
+console.log(arr)
+
+
+///Question 2
+let a=arr.filter((value)=>{
+    return value%10== 0
+})
+console.log(a)
+let num
+let a1= arr.map((num)=>{
+    return num*num
+})
+console.log(a1)
+
+//Question 3
+
+let arr2 = [1,2,3,4,5,6]
+
+let a3 = arr2.reduce((h1,h2) =>
+{
+    return h1*h2
+})
+console.log(a3)
+console.log(arr2)
+
