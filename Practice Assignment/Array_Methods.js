4bf4062382b8d25df5de51ac4b2d7232d9f058ee
RautMaya <suryawanshimaya28@gmail.com>
2025-03-20 20:03:23 +0530

    Array Methods

--- Practice Assignment/Array_Methods.js
+++ Practice Assignment/Array_Methods.js
@@ -27,9 +27,9 @@ let Mark_1 = [11,712,193]
 let Mark_2 = [241555555555,522,723]
 let Mark_3 = [831,312,303]
 
+let Mark_4 = [831,312,303]
 
-
-let New_Array = Mark_1.concat(Mark_2,Mark_3)
+let New_Array = Mark_1.concat(Mark_2,Mark_3,Mark_4)
 console.log(New_Array)
 
 /// Sort
