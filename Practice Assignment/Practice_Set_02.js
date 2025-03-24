291f73ea965a8fda2e519a7e0c0b89761588818d
RautMaya <suryawanshimaya28@gmail.com>
2025-03-24 16:34:11 +0530

    Practice Set

---
+++ Practice Assignment/Practice_Set_02.js
@@ -0,0 +1,23 @@
+/// Q 1
+
+let a = prompt("Enter Name");
+
+let age = Number.parseInt(a)
+
+if(age < 20 )
+{
+    let result =console.log("Do not drive");
+}
+else if(age > 21 || age < 45)
+{
+    result =console.log("You Can Drive Safely");
+}
+else if(age > 46)
+{
+    result =console.log("dont be drive");
+}
+else{
+    result =console.log("Enter Valid Age");
+}
+console.log(result);
+
