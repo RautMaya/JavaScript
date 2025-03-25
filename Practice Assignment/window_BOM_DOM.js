9a2969a8bb101e06f9ee61eb11052f504d796169
RautMaya <suryawanshimaya28@gmail.com>
2025-03-25 12:40:37 +0530

    DOM Concept

---
+++ Practice Assignment/window_BOM_DOM.js
@@ -0,0 +1,10 @@
+window.console.log(window);
+
+///DOM
+console.log(document.body);
+
+console.log(document.body.style.background = "yellow");
+
+///BOM
+
+location.href = "https://github.com/RautMaya/"
