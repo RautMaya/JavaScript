101fbe21ef53c07cbc3ed13f0c7903bb209eed5d
RautMaya <suryawanshimaya28@gmail.com>
2025-03-25 12:36:25 +0530

    Console Object Method

---
+++ Practice Assignment/Console_Object_Methods.js
@@ -0,0 +1,14 @@
+/// This script run ==== it may be join to html and then this html is open it on browser
+
+
+let Obj = [55,12,33,5];
+console.table(Obj);
+
+console.warn("Heyy it is an Warning");
+
+console.error("Heyy it is error");
+
+console.info("Here is it information");
+
+console.assert("error" !=false);
+console.assert("error" ==false);
