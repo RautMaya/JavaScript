44da15a53308cdb6806a0f13af152e8d3941e974
RautMaya <suryawanshimaya28@gmail.com>
2025-03-28 16:55:48 +0530

    Interaction

--- Practice Assignment/Interaction.js
+++ Practice Assignment/Interaction.js
@@ -1,11 +1,11 @@
 alert("Enter Your Name");
 let Name = prompt("Enter Name" , "BCS Student");
-alert("Your name Type" + typeof(Name))
+alert("Your name Type" + typeof(Name));
 
-let Write = confirm("Do you want to Write")
+let Write = confirm("Do you want to Write");
 if(Write){
     document.writeln(Name);
 }
 else{
-    document.writeln("Allow Me to Show your text")
+    document.writeln("Allow Me to Show your text");
 }
