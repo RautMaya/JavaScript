1b4a7b297450b8f2464629c0852e9a4a25b8d1a6
RautMaya <suryawanshimaya28@gmail.com>
2025-03-30 19:16:15 +0530

    String

--- Practice Assignment/String.js
+++ Practice Assignment/String.js
@@ -1,38 +1,38 @@
-let name = "MyFriend"
-console.log(name.length)
+let name = "MyFriend";
+console.log(name.length);
 
 /// To Print Letter
 
-console.log(name[0])
-console.log(name[1])
-console.log(name[2])
-console.log(name[3])
-console.log(name[4])
-console.log(name[5])
-console.log(name[6])
+console.log(name[0]);
+console.log(name[1]);
+console.log(name[2]);
+console.log(name[3]);
+console.log(name[4]);
+console.log(name[5]);
+console.log(name[6]);
 
 
 
-let Friend = 'Pranita'
-console.log(Friend.length)
-console.log(Friend)
+let Friend = 'Pranita';
+console.log(Friend.length);
+console.log(Friend);
 
 
 //// Template Litrals
 
-let boy1 = "Manju"
-let boy2 = "Anju"
+let boy1 = "Manju";
+let boy2 = "Anju";
 
-let Sentence = `${boy1} is a Friend of ${boy2}`
-console.log(Sentence)
+let Sentence = `${boy1} is a Friend of ${boy2}`;
+console.log(Sentence);
 
 ///Escape Sequence 
 
-let Fruit = 'Banan\'s'
-Fruit = 'Man\ngo'
-Fruit = 'Man\rgo'
+let Fruit = 'Banan\'s';
+Fruit = 'Man\ngo';
+Fruit = 'Man\rgo';
 
 
-console.log(Fruit)
+console.log(Fruit);
 
 
