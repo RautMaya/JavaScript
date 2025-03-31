5ae599a138c2c10983a19d12abee705e9d0c3b80
RautMaya <suryawanshimaya28@gmail.com>
2025-03-31 16:54:27 +0530

    Table Navigation

---
+++ Practice Assignment/Table_Navigation.js
@@ -0,0 +1,15 @@
+let t = document.body.firstElementChild;
+console.log(t);
+
+let tb = document.body.firstElementChild.firstElementChild;
+console.log(t);
+
+let tr = document.body.firstElementChild.firstElementChild.nextElementSibling;
+console.log(tr);
+
+let r =console.log(tb.rows);
+console.log(r);
+
+console.log(tb.tBodies);
+console.log(tb.rows[2].rowIndex);
+
