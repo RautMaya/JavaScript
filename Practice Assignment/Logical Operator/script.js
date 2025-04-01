bb0638333e26238986a8f5f19c9b1a3dd4925fa9
RautMaya <suryawanshimaya28@gmail.com>
2025-04-01 22:38:53 +0530

    js file

---
+++ Practice Assignment/Logical Operator/script.js
@@ -0,0 +1,23 @@
+const User1= 25
+const user2=18
+
+const andOp = (User1 >20) &&( User1 < 26)
+
+const OrOp = (User1 <20) ||( User1 == 26)
+
+const andResult = 0 && 2
+const orResult = 0 || 2
+
+const andResult1 = Boolean(andResult)  ///o is false 
+const OrResult1 = Boolean(orResult)
+
+const andResult2 = 'Hello' && 2+5
+const orResult2 = 'Hello' || 2+5
+
+const andResult3 = 2+5 && 'Hello'
+const orResult3 = 2+5 || 'Hellow'
+
+Result = console.log('Hello')
+
+const andResult4 = 2+5 && console.log('Hello')
+const orResult4 = null ||console.log('Hello')
