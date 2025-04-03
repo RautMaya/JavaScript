352201339c656bd2d2490f25ffcbfa71e166a057
RautMaya <suryawanshimaya28@gmail.com>
2025-04-03 22:23:18 +0530

    Map Filter Reduce

--- Practice Assignment/Map_Filter_Reduce.js
+++ Practice Assignment/Map_Filter_Reduce.js
@@ -1,41 +1,41 @@
 /// Map Method
-const arr =[25,36,24]
+const arr =[25,36,24];
 
 let a= arr.map((value,index,array) =>
 {
-  console.log(value,index,array)
-  return value +1
+  console.log(value,index,array);
+  return value +1;
 })
-console.log(a)
+console.log(a);
 
 /// Filter Method 
 
-let arr2 = [25,63,6,8,95,4]
+let arr2 = [25,63,6,8,95,4];
 
 let a2 = arr2.filter((NewArray) => 
 {
-    return NewArray <10
+    return NewArray <10;
 })
 console.log(a2)
 
 /// Reduce Method
 
-let arr3=[1,2,3,5,4]
+let arr3=[1,2,3,5,4];
 
 let Newarr = arr3.reduce((element1 , element2) =>
      {
-    return element1+element2
+    return element1+element2;
 })
-console.log(Newarr)
+console.log(Newarr);
 
 /// Example 2 Reduce_Method
-let arr4=[2,3,2,2]
+let arr4=[2,3,2,2];
 
 const Reduce_Fun =(a,b)=>
 {
-    return a*b
+    return a*b;
 }
-let NewArray1 = arr4.reduce(Reduce_Fun)
-console.log(NewArray1)
+let NewArray1 = arr4.reduce(Reduce_Fun);
+console.log(NewArray1);
 
 
