9f17319bd4a129bcef2b2c938a1b4873e4b35e70
RautMaya <suryawanshimaya28@gmail.com>
2025-04-03 22:24:04 +0530

    String Practice

--- Practice Assignment/String_Practice_Set.js
+++ Practice Assignment/String_Practice_Set.js
@@ -1,25 +1,25 @@
-console.log("har\"".length)
+console.log("har\"".length);
 
-const sentence = `In a play ground Gita and Sita are playing a Cricket.`
-const word = `Cricket`
+const sentence = `In a play ground Gita and Sita are playing a Cricket.`;
+const word = `Cricket`;
 
-console.log(`The word "${word}" ${sentence.includes(word) ? 'is': 'is not'} in the Sentsnce`)
+console.log(`The word "${word}" ${sentence.includes(word) ? 'is': 'is not'} in the Sentsnce`);
 
-console.log(`The sentence "${word}" ${sentence.startsWith(word) ?'is start' : 'is not start'} in the sentence`)
+console.log(`The sentence "${word}" ${sentence.startsWith(word) ?'is start' : 'is not start'} in the sentence`);
 
-console.log(`The sentence "${word}" ${sentence.endsWith(word)? 'is end' : 'is not end' }in The Sentence`)
+console.log(`The sentence "${word}" ${sentence.endsWith(word)? 'is end' : 'is not end' }in The Sentence`);
 
-console.log(sentence.toUpperCase())
+console.log(sentence.toUpperCase());
 
-let sen2 = 'Please give me RS 1000 '
+let sen2 = 'Please give me RS 1000 ';
 
-let amount = sen2.slice(17,22)
-console.log(amount)
-let amount1 = sen2.slice(sen2.length)
-console.log(amount)
+let amount = sen2.slice(17,22);
+console.log(amount);
+let amount1 = sen2.slice(sen2.length);
+console.log(amount);
 
-let Friend = "Deepika"
-console.log(Friend)
+let Friend = "Deepika";
+console.log(Friend);
 
-Friend[1] = "H"
-console.log(Friend)/// In a javascript We are do not change String 
+Friend[1] = "H";
+console.log(Friend);/// In a javascript We are do not change String 
