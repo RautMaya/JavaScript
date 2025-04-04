e4dc24470d28b86e9ebadd4f76db4f9f66073c6b
RautMaya <suryawanshimaya28@gmail.com>
2025-04-04 23:34:02 +0530

    DOM Practice Set JS File

---
+++ Practice Assignment/DOM_Practice_Set.js
@@ -0,0 +1,7 @@
+
+document.getElementsByClassName("a")[0].style.color = "red";
+document.getElementsByClassName("a")[1].style.color = "yellow";
+document.getElementsByClassName("a")[2].style.color = "pink";
+
+
+ 
