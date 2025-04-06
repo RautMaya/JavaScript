056c43087f72340ea04601096d7b6fc01303d68d
RautMaya <suryawanshimaya28@gmail.com>
2025-04-06 22:46:03 +0530

    DOM Practice

--- Practice Assignment/DOM_Practice_Set.js
+++ Practice Assignment/DOM_Practice_Set.js
@@ -1,8 +1,13 @@
 
 document.getElementsByClassName("a")[0].style.color = "red";
 document.getElementsByClassName("a")[1].style.color = "yellow";
-document.getElementsByClassName("a")[2].style.color = "pink";
+document.getElementsByClassName("a")[2].style.color = "red";
 
-let navbar = document.getElementsByTagName("nav");
-navbar.style.backgroundcolor = "black";
+
  
+document.getElementsByTagName("nav")[0].firstElementChild.style.color = "green";
+document.getElementsByTagName("nav")[0].lastElementChild.style.color = "green";
+
+Array.from(document.getElementsByTagName("h3")).forEach(element => {
+    element.style.background = "green";
+});
