4c674d37baac78bd02754148fdb7cf3b6ba5384f
RautMaya <suryawanshimaya28@gmail.com>
2025-04-09 19:05:38 +0530

    Array Methods

--- Practice Assignment/Array_Methods_01.js
+++ Practice Assignment/Array_Methods_01.js
@@ -1,22 +1,22 @@
 ///Sort Method
 
 let Compare = (a,b) => {
-    return a - b
+    return a - b;
 }
 
-let num = [25,69,56,2,89,4,65,1]
+let num = [25,69,56,2,89,4,65,1];
 num.sort(Compare);
-console.log(num)
+console.log(num);
 
-num.reverse()
-console.log(num)
+num.reverse();
+console.log(num);
 
 ///Splice 
-let num2 = [25,69,56,2,89,4,65,1]
+let num2 = [25,69,56,2,89,4,65,1];
 
-num2.splice(2,4,1021,1021,1021,1021)
-console.log(num2)
+num2.splice(2,4,1021,1021,1021,1021);
+console.log(num2);
 
-let num3 = [12,56,98,45,6,3]
-num3.slice(2,6)
-console.log(num3)
+let num3 = [12,56,98,45,6,3];
+num3.slice(2,6);
+console.log(num3);
